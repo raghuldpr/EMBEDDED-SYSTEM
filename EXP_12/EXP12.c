d24eb47f36e4921e314cf57a2d7f12aed3316ac9
Raghul D <raghuldpr@gmail.com>
2026-09-18 17:05:21 +0530

    Refactor multiplication logic and add comments
    Reorganized code for clarity and added comments to explain the multiplication and port assignments.

--- EXP_12/EXP12.c
+++ EXP_12/EXP12.c
@@ -1,11 +1,22 @@
-#include <reg51.h> void main()
+#include <reg51.h>
+
+void main()
 {
-while (1)
-{
-unsigned int num1, num2; unsigned long int product; num1 = 0x2222;
-num2 = 0xBBBB;
-product = (unsigned long int)num1 * num2; P0= product &0xff;
-P1= (product & 0xff00)>>8;
-P2 =(product & 0xff0000)>>16; P3 =(product & 0xff000000)>>24;
-}
+    unsigned int num1, num2;
+    unsigned long int product;
+
+    num1 = 0x2222;
+    num2 = 0xBBBB;
+
+    while (1)
+    {
+        // Multiply two 16-bit numbers
+        product = (unsigned long int)num1 * num2;
+
+        // Send 32-bit result to four 8-bit ports
+        P0 = product & 0xFF;
+        P1 = (product >> 8) & 0xFF;
+        P2 = (product >> 16) & 0xFF;
+        P3 = (product >> 24) & 0xFF;
+    }
 }
